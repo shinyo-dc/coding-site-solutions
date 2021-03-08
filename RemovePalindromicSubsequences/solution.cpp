0b0516dc02d356318d8caf9eb9925af7f5356ffa
Chan-Duong Van <45557860+shinyo-dc@users.noreply.github.com>
2021-03-08 15:20:31 +0700

    Create solution.cpp

---
+++ RemovePalindromicSubsequences/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for (int i = 0; i < s.size()/2; i++) {
+            if (s[i] != s[s.size()-1-i])
+                return false;
+        }
+        return true;
+    }
+    
+    int removePalindromeSub(string s) {
+        if (s.size() == 0)
+            return 0;
+        if (isPalindrome(s))
+            return 1;
+        return 2;
+    }
+};
