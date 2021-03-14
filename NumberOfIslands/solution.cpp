a14a69ffa195ec21c970af840d55567265186e3b
Chan-Duong Van <45557860+shinyo-dc@users.noreply.github.com>
2021-03-14 10:08:55 +0700

    Update solution.cpp

--- NumberOfIslands/solution.cpp
+++ NumberOfIslands/solution.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    /**
+     * DFS solution (using stack)
+     */
     void marking(vector<vector<char>>& grid, int row, int col) {
         if (row < 0 || col < 0 || row == grid.size() || col == grid[0].size() || grid[row][col] == '0')
             return;
@@ -17,6 +20,41 @@ public:
                     marking(grid, i, j);
                     count += 1;
                 }
+        }
+        return count;
+    }
+    /**
+     * BFS solution (using queue)
+     */ 
+    bool isValid(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row >= grid.size() || col < 0 || col >= grid[0].size() || grid[row][col] == '0')
+            return false;
+        return true;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        vector<pair<int, int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    queue<pair<int, int>> q;
+                    q.push({i,j});
+                    grid[i][j] = '0';
+                    while (!q.empty()) {
+                        pair<int, int> front = q.front();
+                        q.pop();
+                        for (pair<int, int> dir : dirs) {
+                            int new_row = front.first + dir.first;
+                            int new_col = front.second + dir.second;
+                            if (isValid(grid, new_row, new_col)) {
+                                q.push({new_row, new_col});
+                                grid[new_row][new_col] = '0';
+                            }
+                        }
+                    }
+                }
+            }
         }
         return count;
     }
